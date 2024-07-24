6ac8b2cb3ef151022b00a775fe8686b81059af5c
AlexDismo <dismokotik@gmail.com>
2024-07-24 16:33:10 +0300

    fix style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen w-screen flex items-center justify-center bg-gray-100">
+  <div class="h-screen flex items-center justify-center bg-gray-100">
     <div class="w-full h-full p-6 bg-white shadow rounded-3xl">
       <h2 class="text-xl font-semibold text-gray-700 mb-4">Comments</h2>
       <p class="text-sm text-gray-500 font-normal mb-6">Leave your comment below.</p>
